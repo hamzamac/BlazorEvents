ff7ccb8a56cd5b2c13d68e1374a37f36fd8bf747
Makame Makame <makame.makame@knowit.se>
2024-02-20 11:56:28 +0100

    Add project files.

---
+++ Components/CountState.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace EventsBroker.Components
+{
+    public class CountState
+    {
+
+        private HashSet<CountStateChangedSubscription> _changeSubscriptions = new();
+
+        public IDisposable Subscribe(EventCallback callback)
+        {
+            var subscription =new  CountStateChangedSubscription(this, callback);
+            _changeSubscriptions.Add(subscription);
+            return subscription;
+        }
+
+        private Task PublishAsync() => Task.WhenAll(_changeSubscriptions.Select(s => s.NotifyAsync()));
+
+        public Task Refresh () => PublishAsync();
+
+        private class CountStateChangedSubscription(CountState Owner, EventCallback Callback) : IDisposable
+        {
+            public Task NotifyAsync() => Callback.InvokeAsync();
+            public void Dispose() => Owner._changeSubscriptions.Remove(this);
+        }
+    }
+}
---
+++ Services/Counter.cs
@@ -0,0 +1,21 @@
+﻿namespace EventsBroker.Services
+{
+    public class Counter
+    {
+        public event EventHandler? CounterChanged;
+
+        protected virtual void OnCounterChanged(EventArgs e)
+        {
+            CounterChanged?.Invoke(this, e);
+        }
+
+        //public delegate void ThresholdReachedEventHandler(object sender, ThresholdReachedEventArgs e){
+        //    }
+
+        class ThresholdReachedEventArgs : EventArgs
+        {
+            public int Value { get; set; }
+        }
+
+    }
+}
